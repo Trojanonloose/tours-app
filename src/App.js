c2341d589e89035e72239e1357635115cc7855bb
Trojanonloose <shreyasthakare5@gmail.com>
2022-09-28 15:06:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Loading from './Loading';
+import Tours from "./Tours";
+
+const url = 'https://course-api.com/react-tours-project';
 
 function App() {
+
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter(tour => tour.id !== id)
+    setTours(newTours)
+  }
+
+  async function fetchUsers() {
+    setLoading(true)
+    try {
+      await axios.get(url)
+        .then((response) => setTours(response.data))
+        .then(setLoading(false))
+        .catch(setLoading(false))
+    }
+    catch (err) {
+      console.error(err)
+    }
+  }
+
+  useEffect(() => {
+    fetchUsers()
+  }, [])
+
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    )
+  }
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <button className='refresh-btn' onClick={() => fetchUsers()}>refresh</button>
+        </div>
+      </main>
+    )
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  )
 }
 
 export default App;
